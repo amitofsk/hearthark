2f60301778ced112083b1dbda9480c3b079a3357
sudett <sudett4978@gmail.com>
2022-01-30 02:30:45 +0330

    added user personal info to the patient page

--- src/pages/patient-page/patient-page.component.jsx
+++ src/pages/patient-page/patient-page.component.jsx
@@ -23,24 +23,77 @@ const PatientPage = () => {
 
   return (
     <div className="patient-page">
-      <article className="heart-rate">
-        <span>Current heart rate</span>
-        <div className="heart-rate__info">
-          <p>
-            <span className="heart-rate__number">124</span> bpm
-          </p>
-          <FaHeartbeat className="heart-rate__icon" />
-        </div>
-      </article>
-
-      <article className="RHR-container">
-        <h3 className="heading-tertiary chart__heading">Resting heart rate</h3>
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart options={options} series={series} type="line" width="400" />
+      <div className="user-charts">
+        <article className="heart-rate">
+          <span>Current heart rate</span>
+          <div className="heart-rate__info">
+            <p>
+              <span className="heart-rate__number">124</span> bpm
+            </p>
+            <FaHeartbeat className="heart-rate__icon" />
           </div>
-        </div>
-      </article>
+        </article>
+
+        <article className="RHR-container">
+          <h3 className="heading-tertiary chart__heading">
+            Resting heart rate
+          </h3>
+          <div className="row">
+            <div className="mixed-chart">
+              <Chart
+                options={options}
+                series={series}
+                type="line"
+                width="400"
+              />
+            </div>
+          </div>
+        </article>
+      </div>
+
+      {/* User Info */}
+      <div className="user-info">
+        <article>
+          <div className="user__main-info">
+            <div className="user__image"></div>
+            <div>
+              <h2 className="heading-secondary">Issac williams</h2>
+              <h3 className="heading-tertiary">25 years old, London</h3>
+            </div>
+          </div>
+
+          <div className="user__details">
+            <div className="user__details-group">
+              <div className="user__details-text">
+                <span className="user__details-label">blood</span>
+                <span className="user__details-value">O+</span>
+              </div>
+            </div>
+            <div className="user__details-group">
+              <div className="user__details-text">
+                <span className="user__details-label">height</span>
+                <span className="user__details-value">186cm</span>
+              </div>
+            </div>
+            <div className="user__details-group">
+              <div className="user__details-text">
+                <span className="user__details-label">weight</span>
+                <span className="user__details-value">90kg</span>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <article className="appointments">
+          <div className="appointments__container">
+            <h3 className="appointments__heading">Health appointments</h3>
+            <span>9:00AM - 11:30AM</span>
+            <span className="appointments__doctor">dr. douglas wade</span>
+            <hr />
+            <button className="btn">follow-up</button>
+          </div>
+        </article>
+      </div>
     </div>
   );
 };
