841d210f208c55ac0651631fb1ce9b44f108c7b5
= <amamitof7@gmail.com>
2022-02-06 10:20:42 -0500

    fft and heroku don't work together

--- src/pages/patientAM-page/patientAM-page.component.jsx
+++ src/pages/patientAM-page/patientAM-page.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {} from "fft-js";
-import {} from "wavefile";
+//import {} from "fft-js";
+//import {} from "wavefile";
 
 class PatientAMPage extends React.Component{
 	render() {
